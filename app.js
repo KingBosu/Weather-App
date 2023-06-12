7d1e2b8f006dd3ace7f1a82c8f05eb6072d8e45b
KingBosu <mckaywilliamiii@gmail.com>
2023-06-12 17:22:19 -0400

    Fixed API calls and rearranged display

--- app.js
+++ app.js
@@ -8,32 +8,25 @@ weatherForm.addEventListener('submit', async (event) => {
   const city = document.querySelector('#cityInput').value;
   const apiKey = 'af555ad7b47f68538f1b3097697556e6';
 
-  const { lat, lon } = await convertCityToLongAndLat(city, apiKey);
-  const weatherData = await getWeatherData(lat, lon, apiKey);
-  displayWeatherData(weatherData);
+  const weatherData = await getWeatherData(city, apiKey);
+  displayWeatherData(weatherData,city);
 });
 
-async function convertCityToLongAndLat(city, apiKey) {
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)},${state},${zipcode}&limit=1&appid=${apiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const latitude = data[0].lat;
-  const longitude = data[0].lon;
-  return { lat: latitude, lon: longitude };
-}
 
-async function getWeatherData(lat, lon, apiKey) {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}`;
+
+async function getWeatherData(city, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
   const response = await fetch(url);
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
-function displayWeatherData(weatherData) {
+function displayWeatherData(weatherData,city) {
+
   weatherResult.innerHTML = `
-    <h2>Weather for ${weatherData.timezone}</h2>
-    <p>Temperature: ${weatherData.current.temp_c}°C</p>
-    <p>Condition: ${weatherData.current.weather[0].description}</p>
-    <p>Humidity: ${weatherData.current.humidity}%</p>
+    <h2> Weather for ${city}</h2>
+    <p>Weather Condition  ${weatherData.weather[0].description}</h2>
+    <p>Temperature: ${weatherData.main.temp}°C</p>
   `;
 }
